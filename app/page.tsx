20d054c6141f0f6b12b7de95d1c37f9c0ba9cad8
Awadesh <36awadesh@gmail.com>
2024-01-04 20:25:04 +0530

    Home Components skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <section className="flex-start flex-col padding mb-16">
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>LoadMore</h1>
+        </section>
     )
 }
 
