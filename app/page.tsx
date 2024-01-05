3adcb4f18dfe29ce58decc1bf27f08251930cbc0
Awadesh <36awadesh@gmail.com>
2024-01-06 00:18:54 +0530

    page layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ const Home = () => {
         <section className="flex-start flex-col padding mb-16">
             <h1>Categories</h1>
             <h1>Posts</h1>
-            <h1>LoadMore</h1>
+            <h1>Load More</h1>
         </section>
     )
 }
