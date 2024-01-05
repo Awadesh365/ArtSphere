6d0613038d4eac856a276e7c66c6b7c0a61ff60a
Awadesh <36awadesh@gmail.com>
2024-01-06 01:23:52 +0530

    Add Contants directory

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,6 +14,10 @@ const Navbar = () => {
                         alt="artsphere"
                     />
                 </Link>
+                <ul className="xl:flex hidden
+                    text-small gap-7">
+
+                </ul>
             </div>
         </nav >
     )
