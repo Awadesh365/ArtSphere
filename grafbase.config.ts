fc79875ca11c60875354fbd5e89000c58d90daa1
Awadesh <36awadesh@gmail.com>
2024-01-17 21:30:16 +0530

    Add Project Model and complete user model

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,18 +1,26 @@
-import { g, config } from '@grafbase/sdk'
+import { g, auth, config } from '@grafbase/sdk'
 
 const user = g.model('User', {
   name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string(),
+  description: g.string().optional(),
   github: g.url().optional(),
   linkedin: g.url().optional(),
-  project: g.relation(),
+  projects: g.relation(() => project).optional(),
 
 })
 
 
-
+const project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url().optional(),
+  githubUrl: g.url().optional(),
+  category: g.string().search(),
+  createdBy: g.relation(() => user),
+})
 
 export default config({
   schema: g
